5255b0e29ae7b0c29d56e43d03ddfadc896a6ff3
Valdemort <lavrovvoldemar@gmail.com>
2023-10-30 12:22:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+alert("Helllo!")
+let user_answer = confirm("Are you 18 years old?")
+console.log(user_answer)
+let user_str = prompt("What do you think about LGBT?","")
+console.log(user_str)
+
+console.log(`You are write:${user_str}`)
+// let arr = [1,2,3,4,5];
+// console.log(arr[0]);
+
+// arr.push({a:1, b:2})
+
+// console.log(arr)
+
